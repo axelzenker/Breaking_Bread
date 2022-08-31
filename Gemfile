c5e9386ade1d115ba1f677ad33a5dc106725121a
Q Hai Dang <haiqd@pm.me>
2022-08-31 15:18:20 +0200

    lots of stuff except pundit, map and image upload

--- Gemfile
+++ Gemfile
@@ -83,3 +83,5 @@ end
 gem "pundit"
 
 gem "geocoder"
+
+gem "cloudinary"
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,7 +1,7 @@
 class Meal < ApplicationRecord
   belongs_to :user
   has_many :orders
-
+  has_many_attached :photos
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
