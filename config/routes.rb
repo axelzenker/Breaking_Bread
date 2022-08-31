e4cb57dca1e09c88d4d62e117f70f8b3e32ff4d9
Afolabi Afolayan <aaafolayan@gmail.com>
2022-08-31 15:39:02 +0200

    made a dashboard for users

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :meals do
     resources :orders, only: %i[create]
   end
+
+  get "dashboard", to: 'pages#dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
