e2e83f50c4eb96bb79286ce1d6ce78f959d11f25
Q Hai Dang <haiqd@pm.me>
2022-08-31 14:12:47 +0200

    added addresses to meals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_151825) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_31_105215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_151825) do
     t.integer "reservation_min", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.index ["user_id"], name: "index_meals_on_user_id"
   end
 
