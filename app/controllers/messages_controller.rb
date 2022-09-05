37074c8ee5c2f36facccfad74b51063c08b25b70
Q Hai Dang <haiqd@pm.me>
2022-09-05 16:58:12 +0200

    restoring correct css displaying, implementing chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,8 @@ class MessagesController < ApplicationController
     if @message.save
       OrderChannel.broadcast_to(
         @order,
-        render_to_string(partial: "message", locals: {message: @message})
+        message: render_to_string(partial: "message", locals: { message: @message }),
+        sender_id: @message.user.id
       )
       head :ok
 
